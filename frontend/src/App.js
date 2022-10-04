eec5cffcc41349495752e164088bc3e4247e5f0d
Lac Vinh Do <lacvinhdo@gmail.com>
2022-10-04 21:29:39 +0100

    commit new version of react-bootstrap and bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
+import Search from './components/Search';
+
 
 const App = () => {
   return (
     <div>
       <Header title="Movies Gallery"/>
+      <Search />
     </div>
   );
 }
